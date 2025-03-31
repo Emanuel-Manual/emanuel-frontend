427674026642613b9d04f6f7ae6ae68f71747b6f
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-03-31 15:49:37 +0200

    use data

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -100,22 +100,17 @@
 <main>
   <div class=" min-h-screen bg-gray-100 p-4">
     <div class="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-      <div
-        class="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4"
-      >
-        <Card.Root
-          class="sm:col-span-2"
-          data-x-chunk-name="dashboard-05-chunk-0"
-          data-x-chunk-description="A card for an orders dashboard with a description and a button to create a new order."
-        >
+      <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
+        <!-- General Overview Card -->
+        <Card.Root class="sm:col-span-2" data-x-chunk-name="dashboard-05-chunk-0" data-x-chunk-description="A card for an orders dashboard with a description and a button to create a new order.">
           <Card.Header class="pb-3">
-            <Card.Title>DigiFunk 1</Card.Title>
+            <Card.Title>{data.general.title}</Card.Title>
             <Card.Description class="max-w-lg text-balance leading-relaxed">
-              2,4 GHz Funkübertragungssystem
+              {data.general.subtitle}
               <br />
-              Deutsch, Englisch
+              {data.general.languages}
               <br />
-              356 pages
+              {data.general.pages} pages
             </Card.Description>
           </Card.Header>
           <Card.Footer>
@@ -125,13 +120,12 @@
             </Button>
           </Card.Footer>
         </Card.Root>
-        <Card.Root
-          data-x-chunk-name="dashboard-05-chunk-1"
-          data-x-chunk-description="A stats card showing this week's total sales in USD, the percentage difference from last week, and a progress bar."
-        >
+
+        <!-- Score Card -->
+        <Card.Root data-x-chunk-name="dashboard-05-chunk-1" data-x-chunk-description="A stats card showing score.">
           <Card.Header class="pb-2">
             <Card.Description>Score</Card.Description>
-            <Card.Title class="text-4xl">89/100</Card.Title>
+            <Card.Title class="text-4xl">{data.general.score}/100</Card.Title>
           </Card.Header>
           <Card.Content>
             <div class="text-muted-foreground text-xs">Needs improvement</div>
@@ -140,13 +134,12 @@
             <Progress value={25} aria-label="25% increase" />
           </Card.Footer>
         </Card.Root>
-        <Card.Root
-          data-x-chunk-name="dashboard-05-chunk-2"
-          data-x-chunk-description="A stats card showing this month's total sales in USD, the percentage difference from last month, and a progress bar."
-        >
+
+        <!-- Tests Card -->
+        <Card.Root data-x-chunk-name="dashboard-05-chunk-2" data-x-chunk-description="A stats card showing tests.">
           <Card.Header class="pb-2">
             <Card.Description>Tests</Card.Description>
-            <Card.Title class="text-3xl">134/250</Card.Title>
+            <Card.Title class="text-3xl">{data.general.tests_passed}/{data.general.tests}</Card.Title>
           </Card.Header>
           <Card.Content>
             <div class="text-muted-foreground text-xs">53% of tests passed</div>
@@ -156,6 +149,8 @@
           </Card.Footer>
         </Card.Root>
       </div>
+
+      <!-- Tabs -->
       <Tabs.Root value="personas">
         <div class="flex">
           <Tabs.List>
@@ -177,113 +172,77 @@
             </Tabs.Trigger>
           </Tabs.List>
         </div>
+
+        <!-- Personas Tab -->
         <Tabs.Content value="personas">
           <Card.Root class="">
-            <!-- <Card.Header>
-          <Card.Title>Personas</Card.Title>
-          <Card.Description>All personas.</Card.Description>
-        </Card.Header> -->
             <Card.Content>
               <Accordion.Root class="w-full">
-                <Accordion.Item value="item-1">
+                {#each data.personas as persona, i}
+                <Accordion.Item value={"item-" + i}>
                   <Accordion.Trigger>
                     <div class="flex items-start gap-4 text-left w-full pr-4">
                       <Avatar.Root>
-                        <Avatar.Image
-                          src="https://thispersondoesnotexist.com"
-                        />
-                        <Avatar.Fallback>BB</Avatar.Fallback>
+                        <Avatar.Image src="https://thispersondoesnotexist.com" />
+                        <!-- derive initials from first and last name -->
+                        <Avatar.Fallback>
+                          {persona.name.split(' ')[0][0]}{persona.name.split(' ')[1] ? persona.name.split(' ')[1][0] : ''}
+                        </Avatar.Fallback>
                       </Avatar.Root>
                       <div class="grid gap-1">
-                        <p class="text-sm font-medium leading-none">
-                          Beate Bumsbaum
-                        </p>
-                        <p class="text-muted-foreground text-sm">
-                          55 Years | Non Technical | At home
-                        </p>
+                        <p class="text-sm font-medium leading-none">{persona.name}</p>
+                        <p class="text-muted-foreground text-sm">{persona.age} Years | {persona.short_description} | {persona.current_setting}</p>
                       </div>
-                      <div
-                        class="ml-auto font-medium bg-gray-200 rounded-full p-2 px-3"
-                      >
-                      <div class="flex justify-center mx-1">
-                        <Smile class="h-4 w-4" />
-                        <p class="text-xs mx-1">22%</p>
-                   
-                        <CircleHelp class="ml-5 h-4 w-4" />
-                        <p class="text-xs mx-1">55 / 77</p>
-                      </div>
-                      
+                      <div class="ml-auto font-medium bg-gray-200 rounded-full p-2 px-3">
+                        <div class="flex justify-center mx-1">
+                          <Smile class="h-4 w-4" />
+                          <p class="text-xs mx-1">{Math.round(persona.total_satisfaction * 100)}%</p>
+                          <CircleHelp class="ml-5 h-4 w-4" />
+                          <p class="text-xs mx-1">{persona.answered_questions} / {persona.total_questions}</p>
+                        </div>
                       </div>
                     </div>
                   </Accordion.Trigger>
                   <Accordion.Content>
                     <div class="w-full rounded bg-gray-100 p-2 mb-2">
-                      Beate is a 55-year-old non-technical user who uses the DigiFunk 1 system at home. She is not very tech-savvy and needs a simple and intuitive interface to operate the system. Beate is looking for a user-friendly manual that can help her understand the basic functions of the system without overwhelming her with technical jargon.
-
+                      {persona.short_scenario}
                     </div>
-                    <div class="w-full">
-                      <Separator />
-                      <div
-                        class="flex flex-row justify-start items-center gap-2 hover:bg-gray-100 p-2"
-                      >
-                        <div
-                          class="flex flex-row items-center bg-gray-200 rounded-full w-fit"
-                        >
-                          <div class="bg-green-200 rounded-full w-fit p-2">
-                            <CircleCheck class="h-4 w-4 white" />
+                    {#if persona.questions && persona.questions.length}
+                      {#each persona.questions as question}
+                      <div class="w-full">
+                        <Separator />
+                        <div class="flex flex-row justify-start items-center gap-2 hover:bg-gray-100 p-2">
+                          <div class="flex flex-row items-center bg-gray-200 rounded-full w-fit">
+                            {#if question.answered}
+                              <div class="bg-green-200 rounded-full w-fit p-2">
+                                <CircleCheck class="h-4 w-4" />
+                              </div>
+                            {:else}
+                              <div class="bg-red-200 rounded-full w-fit p-2">
+                                <CircleAlert class="h-4 w-4" />
+                              </div>
+                            {/if}
+                            <div class="flex justify-center mx-1">
+                              <Smile class="h-4 w-4" />
+                              <p class="text-xs mx-1">{Math.round(question.satisfaction * 100)}%</p>
+                            </div>
                           </div>
-                          <div class="flex justify-center mx-1">
-                            <Smile class="h-4 w-4" />
-                            <p class="text-xs mx-1">77%</p>
+                          <div class="text-sm font-medium leading-none">
+                            {question.question}
                           </div>
-                        </div>
-                        <div class="text-sm font-medium leading-none">
-                          Find volume settings in the menu bar
                         </div>
                       </div>
-                    </div>
-                    <div class="w-full">
-                      <Separator />
-                      <div
-                        class="flex flex-row justify-start items-center gap-2 hover:bg-gray-100 p-2"
-                      >
-                        <div
-                          class="flex flex-row items-center bg-gray-200 rounded-full w-fit"
-                        >
-                          <div class="bg-red-200 rounded-full w-fit p-2">
-                            <CircleAlert class="h-4 w-4" />
-                          </div>
-                          <div class="flex justify-center mx-1">
-                            <Smile class="h-4 w-4" />
-                            <p class="text-xs mx-1">22%</p>
-                          </div>
-                        </div>
-
-                        <div class="text-sm font-medium leading-none">
-                          Find volume settings in the menu bar
-                        </div>
-                      </div>
-                    </div>
+                      {/each}
+                    {/if}
                   </Accordion.Content>
                 </Accordion.Item>
-                <Accordion.Item value="item-2">
-                  <Accordion.Trigger>Is it styled?</Accordion.Trigger>
-                  <Accordion.Content>
-                    Yes. It comes with default styles that matches the other
-                    components' aesthetic.
-                  </Accordion.Content>
-                </Accordion.Item>
-                <Accordion.Item value="item-3">
-                  <Accordion.Trigger>Is it animated?</Accordion.Trigger>
-                  <Accordion.Content>
-                    Yes. It's animated by default, but you can disable it if you
-                    prefer.
-                  </Accordion.Content>
-                </Accordion.Item>
+                {/each}
               </Accordion.Root>
             </Card.Content>
           </Card.Root>
         </Tabs.Content>
+
+        <!-- Technical Tab -->
         <Tabs.Content value="technical">
           <Card.Root>
             <Card.Header class="p-4">
@@ -292,6 +251,8 @@
             </Card.Header>
           </Card.Root>
         </Tabs.Content>
+
+        <!-- Writing Style Tab -->
         <Tabs.Content value="style">
           <Card.Root>
             <Card.Header class="p-4">
@@ -300,6 +261,8 @@
             </Card.Header>
           </Card.Root>
         </Tabs.Content>
+
+        <!-- Tips Tab -->
         <Tabs.Content value="tips">
           <Card.Root>
             <Card.Header class="p-4">
@@ -308,6 +271,7 @@
             </Card.Header>
           </Card.Root>
         </Tabs.Content>
+
       </Tabs.Root>
     </div>
   </div>
