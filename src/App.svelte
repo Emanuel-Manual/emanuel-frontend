4949573b9a75baa70217a20975227356de283db5
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-03-31 12:08:45 +0200

    add shadcn

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import "./app.css";
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import "./app.pcss";
 </script>
 
 <main>
